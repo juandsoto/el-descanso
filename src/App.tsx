a2c38e35df1dc8e42c243a5b4592ebafe3d59d08
juandsoto <juandsoto1006@gmail.com>
2022-04-14 10:00:17 -0500

    starter

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+	return (
+		<>
+			<h1>El Descanso</h1>
+			<ul>
+				<li>Natalia Andrea Giraldo Erazo</li>
+				<li>Juan David Soto Carmona</li>
+				<li>Jorge Adrian Martinez Gomez</li>
+				<li>Mauricio Rodriguez Ramirez</li>
+				<li>Andres Julian Gonzalez Barrera</li>
+				<li>Juan Manuel Perez Alvarez</li>
+			</ul>
+		</>
+	);
+}
+
+export default App;
